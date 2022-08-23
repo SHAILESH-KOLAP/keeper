0ee61704dd4238f929598918780b5efb763e8635
Shailesh Kolap <skolap22@gmail.com>
2022-08-23 10:22:45 +0530

    Added Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Header from "./Header";
+import Footer from "./Footer";
 
-function App(){
-    return(
-        <Header />
-    )
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
